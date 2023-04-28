0f5e0b9fa1ce85c563a88e6c04985e0f30b23368
Ryan Rizeq <ryanrizeq@gmail.com>
2023-04-28 16:14:03 -0700

    Begin search by category event listener

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    // Event Listener for 
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(Object.values(data));
+        })
 })
