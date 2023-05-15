9dee36b024e7eb8035fe83659a6a9a9eb8077d63
Ryan Rizeq <ryanrizeq@gmail.com>
2023-05-14 17:09:06 -0700

    Add event listener for Search by Name form submission

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Event Listener for 
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(Object.values(data));
-        })
+    // Event Listener for Search by Cocktail Name
+    const searchByName = document.getElementById('search-by-name');
+    searchByName.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        console.log('I have been searched!')
+    })
 })
+
+    // fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=tequila")
+    //     .then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(data) {
+    //         console.log(Object.values(data));
+    //     })
