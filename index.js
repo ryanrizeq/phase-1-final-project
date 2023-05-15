e534cbf79dda15cc26113d04fb687b79c529b022
Ryan Rizeq <ryanrizeq@gmail.com>
2023-05-14 18:13:16 -0700

    display results from search by name

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchByName = document.getElementById('search-by-name');
     searchByName.addEventListener('submit', (event) => {
         event.preventDefault()
-        console.log('I have been searched!')
+        // Save search information
+        const searchValue = searchByName.elements['drink-by-name'].value;
+
+        // Fetch data using saved search information
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                const results = Object.values(data)[0];
+                const display = document.getElementById('search-results');
+                
+                for (const element of results) {
+                    // Display drink name list of results
+                    const drinkName = document.createElement('li');
+                    drinkName.innerText = element['strDrink'];
+                    display.appendChild(drinkName);
+                }
+
+                // 
+            })
     })
 })
-
-    // fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=tequila")
-    //     .then(function(response) {
-    //         return response.json();
-    //     })
-    //     .then(function(data) {
-    //         console.log(Object.values(data));
-    //     })
