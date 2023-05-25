0c96a8baf00115992c0f3388b3c9d2faadbc6c94
Ryan Rizeq <ryanrizeq@gmail.com>
2023-05-24 18:38:21 -0700

    removes all comments for assessment

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Variable declaration
     const searchByName = document.getElementById('search-by-name');
     const resultsDisplay = document.getElementById('results-display');
     
-    // Event Listener for form submission
     searchByName.addEventListener('submit', (event) => {
         event.preventDefault()
         
-        // Save search information
         const searchValue = searchByName.elements['drink-by-name'].value;
         
-        // Delete existing search results
         document.getElementById('search-results').remove()
 
-        // Create Search Results Heading
-        // const searchHeading = document.createElement('h3').innerText = 'Search Results';
-        // resultsDisplay.append(searchHeading);
-
-        // Create unordered list
         const display = document.createElement('ul');
         display.id = 'search-results';
         resultsDisplay.append(display);
 
-        // Fetch data using saved search information
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
             .then(function(response) {
                 return response.json();
@@ -30,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(function(data) {
                 const results = Object.values(data)[0];
                 
-                // Filter results to show drinks with three ingredients or less
                 const filteredResults = results.filter(function (element) {
                     if (element['strIngredient4'] === null) {
                         return element;
@@ -43,46 +32,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 } else {
                     for (const element of filteredResults) {
-                        // Display drink name list of results
                         const drinkName = document.createElement('h4');
                         drinkName.innerText = element['strDrink'];
                         display.append(drinkName);
     
-                        // Event listener for drink name hover
                         drinkName.addEventListener('mouseover', () => {
                             drinkName.style.color = '#0049B7';
                         })
     
-                        // Event listener for drink name hover exit
                         drinkName.addEventListener('mouseout', () => {
                             drinkName.style.color = 'black';
                         })
     
-                        // Event listener for click on list of names
                         drinkName.addEventListener('click', () => {
-                            // Create unordered list for drink information
                             const drinkInfo = document.createElement('ul');
                             drinkName.append(drinkInfo);
                             
-                            // Create img
                             const cocktailImage = document.createElement('img');
                             cocktailImage.src = `${element['strDrinkThumb']}`;
                             cocktailImage.id = 'image'
                             drinkInfo.appendChild(cocktailImage);
 
-                            // Display drink glass information
                             const glassInfo = document.createElement('li');
                             glassInfo.innerText = `Drink Glass: ${element['strGlass']}`;
                             drinkInfo.appendChild(glassInfo); 
     
-                            // Create Ingredients information shell
                             const ingredients = document.createElement('li');
                             ingredients.innerText = 'Ingredients';
                             const ingredientsList = document.createElement('ul');
                             drinkInfo.appendChild(ingredients);
                             ingredients.appendChild(ingredientsList);
     
-                            // Display first ingredient
                             const firstIngredient = document.createElement('li');
                             if (element['strMeasure1'] !== null) {
                                 firstIngredient.innerText = `${element['strMeasure1']} ${element['strIngredient1']}`;
@@ -91,7 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
                             }
                             ingredientsList.appendChild(firstIngredient);
     
-                            // Display second ingredient
                             const secondIngredient = document.createElement('li');
                             if (element['strIngredient2'] !== null) {
                                 if (element['strMeasure2'] !== null) {
@@ -102,7 +81,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                 ingredientsList.appendChild(secondIngredient);
                             }
     
-                            // Display third ingredient
                             const thirdIngredient = document.createElement('li');
                             if (element['strIngredient3'] !== null) {
                                 if (element['strMeasure3'] !== null) {
@@ -113,7 +91,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                 ingredientsList.appendChild(thirdIngredient);
                             }
     
-                            // Display instructions information
                             const instructions = document.createElement('li');
                             instructions.innerText = `Instructions: ${element['strInstructions']}`;
                             drinkInfo.appendChild(instructions);
